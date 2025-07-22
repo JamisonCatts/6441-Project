8d2a295ad00fab4bd7783d56f4e3c79c4d6c7c4c
JamisonCatts <jamison.catts@gmail.com>
2025-07-22 17:47:46 +1000

    Added files manually

---
+++ main.cpp
@@ -0,0 +1,61 @@
+
+#include <iostream>
+
+void run_calculator();
+int get_result(int &a, int &b, char & op);
+
+
+
+int main(){
+    
+    run_calculator();
+
+
+return 0;
+
+}
+
+
+void run_calculator(){
+
+
+    int a, b;
+    char op;
+
+std::cout << "Enter two numbers" << std::endl;
+
+std::cin >> a >> b;
+
+std::cout << "Enter operation" << std::endl;
+
+
+std::cin >> op;
+
+std::cout << std::endl;
+
+std::cout << get_result(a, b, op) << std::endl;
+
+
+
+}
+
+
+int get_result(int &a, int &b, char & op){
+
+    switch(op){
+
+    case '+': return a + b;
+    case '*': return a * b;
+    case '-': return a - b;
+    case '/': 
+        if (b != 0){
+
+            return a / b;
+        }
+        else {
+            throw("Error: division by 0");
+            
+        }
+    }
+    return 0;
+}
